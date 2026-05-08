296081d6dcdc41d732f3266c22dd6179baf56347
Cristian Malhue <cris.malhue@duocuc.cl>
2026-05-07 20:17:50 -0400

    agregue la lista de tipos

--- src/main/java/com/SoftwareAviones/SoftwareAviones/model/Tipo.java
+++ src/main/java/com/SoftwareAviones/SoftwareAviones/model/Tipo.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,10 +30,9 @@ public class Tipo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_tipo;
 
-    @NotBlank(message = "Debe especificarse el tipo del avion")
-    @Size(min = 5, max = 15, message = "El tipo de avion debe tener de 5 a 15 caracteres")
-    @Column(nullable = false, unique = true, length = 15)
-    private String tipo_avion;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private TipoAvion tipo;
 
     @OneToMany(mappedBy = "tipo")
     private List<Avion> aviones;
